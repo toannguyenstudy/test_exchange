a893888b8b61c231df4f4ea5cb76c6480203bd52
unknown <toannguyenstudy@gmail.com>
2019-11-11 16:54:48 +0700

    design open order done 1

--- src/app/components/exchange-open-order/exchange-open-order.component.ts
+++ src/app/components/exchange-open-order/exchange-open-order.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-exchange-open-order',
-  templateUrl: './exchange-open-order.component.html',
-  styleUrls: ['./exchange-open-order.component.scss'],
+    selector: 'app-exchange-open-order',
+    templateUrl: './exchange-open-order.component.html',
+    styleUrls: ['./exchange-open-order.component.scss']
 })
 export class ExchangeOpenOrderComponent implements OnInit {
+    openOrdersSell = [];
+    openOrdersBuy = [1];
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {}
-
+    ngOnInit() {}
 }
