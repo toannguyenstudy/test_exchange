776154cc7097cc2a3060f695c18199164fd8a8dc
unknown <toannguyenstudy@gmail.com>
2019-11-18 22:06:07 +0700

    add and design balance page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,23 +2,27 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-    {
-        path: '',
-        loadChildren: () =>
-            import('./tabs/tabs.module').then(m => m.TabsPageModule),
-    },
-    //   { path: 'wallet', loadChildren: './pages/wallet/wallet.module#WalletPageModule' },
-    {
-        path: 'detail/:coin/:unit',
-        loadChildren:
-            './pages/market-detail/market-detail.module#MarketDetailPageModule',
-    },
+	{
+		path: '',
+		loadChildren: () =>
+			import('./tabs/tabs.module').then(m => m.TabsPageModule),
+	},
+	//   { path: 'wallet', loadChildren: './pages/wallet/wallet.module#WalletPageModule' },
+	{
+		path: 'detail/:coin/:unit',
+		loadChildren:
+			'./pages/market-detail/market-detail.module#MarketDetailPageModule',
+	},
+	{
+		path: 'balance',
+		loadChildren: './pages/balance/balance.module#BalancePageModule',
+	},
 ];
 
 @NgModule({
-    imports: [
-        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
-    ],
-    exports: [RouterModule],
+	imports: [
+		RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+	],
+	exports: [RouterModule],
 })
 export class AppRoutingModule {}
