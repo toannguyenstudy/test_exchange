d4653cdd0633888f9651afdafab95e3a0e9808a7
unknown <toannguyenstudy@gmail.com>
2019-11-18 16:07:17 +0700

    fix iframe src using by pass

--- src/app/pages/market-detail/market-detail.page.ts
+++ src/app/pages/market-detail/market-detail.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { randomNumberInRange, roundNumberNearest } from '../../controls';
 
 @Component({
@@ -8,8 +9,13 @@ import { randomNumberInRange, roundNumberNearest } from '../../controls';
     styleUrls: ['./market-detail.page.scss'],
 })
 export class MarketDetailPage implements OnInit {
+    safeSrc: SafeResourceUrl;
+
     activatedCoin = '';
     activatedUnit = '';
+    iframeSrc =
+        'https://s.tradingview.com/widgetembed/?frameElementId=tradingview_kenniex&interval=60&hidesidetoolbar=1&symboledit=1&saveimage=1&toolbarbg=fff&studies=%5B%5D&theme=Dark&style=2&timezone=Asia%2FBangkok&studies_overrides=%7B%7D&overrides=%7B%7D&enabled_features=%5B%5D&disabled_features=%5B%5D&locale=vi_VN&utm_medium=widget&utm_campaign=chart&symbol=BITTREX%3ABTCUSDT';
+    // iframeSrc = 'https://xem.vn/';
 
     bidOrders = [];
     askOrders = [];
@@ -18,9 +24,14 @@ export class MarketDetailPage implements OnInit {
     constructor(
         private activatedRoute: ActivatedRoute,
         private router: Router,
+        private sanitizer: DomSanitizer,
     ) {}
 
     ngOnInit() {
+        this.safeSrc = this.sanitizer.bypassSecurityTrustResourceUrl(
+            this.iframeSrc,
+        );
+
         this.activatedCoin = this.activatedRoute.snapshot.paramMap.get('coin');
         this.activatedUnit = this.activatedRoute.snapshot.paramMap.get('unit');
 
