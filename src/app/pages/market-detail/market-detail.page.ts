1bbdf77ca2a8f6d844a2f4fcbafd102077643334
unknown <toannguyenstudy@gmail.com>
2019-11-18 15:11:50 +0700

    design market history

--- src/app/pages/market-detail/market-detail.page.ts
+++ src/app/pages/market-detail/market-detail.page.ts
@@ -13,6 +13,8 @@ export class MarketDetailPage implements OnInit {
 
     bidOrders = [];
     askOrders = [];
+
+    marketHistory = [];
     constructor(
         private activatedRoute: ActivatedRoute,
         private router: Router,
@@ -21,34 +23,6 @@ export class MarketDetailPage implements OnInit {
     ngOnInit() {
         this.activatedCoin = this.activatedRoute.snapshot.paramMap.get('coin');
         this.activatedUnit = this.activatedRoute.snapshot.paramMap.get('unit');
-
-        // setTimeout(() => {
-        //     setInterval(() => {
-        //         this.bidOrders = [];
-        //         for (let i = 0; i < randomNumberInRange(1, 10, 0); i++) {
-        //             this.bidOrders.push({
-        //                 price: roundNumberNearest(
-        //                     parseInt(randomNumberInRange(150e6, 250e6, 0)),
-        //                     1e5,
-        //                 ),
-        //                 amount: parseFloat(randomNumberInRange(0.1, 10, 6)),
-        //             });
-        //         }
-        //     }, 3000);
-        // }, 1000);
-
-        // setInterval(() => {
-        //     this.askOrders = [];
-        //     for (let i = 0; i < randomNumberInRange(1, 10, 0); i++) {
-        //         this.askOrders.push({
-        //             price: roundNumberNearest(
-        //                 parseInt(randomNumberInRange(150e6, 250e6, 0)),
-        //                 1e5,
-        //             ),
-        //             amount: parseFloat(randomNumberInRange(0.1, 10, 6)),
-        //         });
-        //     }
-        // }, 3000);
 
         for (let i = 0; i < randomNumberInRange(1, 10, 0); i++) {
             this.bidOrders.push({
@@ -67,6 +41,20 @@ export class MarketDetailPage implements OnInit {
                     1e5,
                 ),
                 amount: parseFloat(randomNumberInRange(0.1, 10, 6)),
+            });
+        }
+
+        for (let i = 0; i < randomNumberInRange(1, 10, 0); i++) {
+            this.marketHistory.push({
+                time: parseInt(
+                    randomNumberInRange(15700000000000, 1579999999999, 0),
+                ),
+                price: roundNumberNearest(
+                    parseInt(randomNumberInRange(150e6, 250e6, 0)),
+                    1e5,
+                ),
+                amount: parseFloat(randomNumberInRange(0.1, 10, 6)),
+                type: randomNumberInRange(1, 2, 0),
             });
         }
     }
