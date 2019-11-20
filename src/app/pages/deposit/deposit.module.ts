4aab8ab50eb6cef1f7a94ad3b61d97b6b5c343be
unknown <toannguyenstudy@gmail.com>
2019-11-20 20:55:54 +0700

    change qrcode from http get to library

--- src/app/pages/deposit/deposit.module.ts
+++ src/app/pages/deposit/deposit.module.ts
@@ -2,25 +2,27 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Routes, RouterModule } from '@angular/router';
+import { QRCodeModule } from 'angularx-qrcode';
 
 import { IonicModule } from '@ionic/angular';
 
 import { DepositPage } from './deposit.page';
 
 const routes: Routes = [
-  {
-    path: '',
-    component: DepositPage
-  }
+    {
+        path: '',
+        component: DepositPage,
+    },
 ];
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    RouterModule.forChild(routes)
-  ],
-  declarations: [DepositPage]
+    imports: [
+        CommonModule,
+        FormsModule,
+        IonicModule,
+        QRCodeModule,
+        RouterModule.forChild(routes),
+    ],
+    declarations: [DepositPage],
 })
 export class DepositPageModule {}
--- src/app/pages/deposit/deposit.page.ts
+++ src/app/pages/deposit/deposit.page.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-deposit',
-  templateUrl: './deposit.page.html',
-  styleUrls: ['./deposit.page.scss'],
+    selector: 'app-deposit',
+    templateUrl: './deposit.page.html',
+    styleUrls: ['./deposit.page.scss'],
 })
 export class DepositPage implements OnInit {
+    address: string = '0x95cFaDDac52b76DA6e858Dd0896a46fcDf79E486';
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit() {
-  }
-
+    ngOnInit() {}
 }
