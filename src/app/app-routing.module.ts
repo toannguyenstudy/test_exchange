4118bc26012380877ae50075cc05cd7aac59e402
unknown <toannguyenstudy@gmail.com>
2019-11-20 09:24:11 +0700

    add deposit page without content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,28 +2,32 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-	{
-		path: '',
-		loadChildren: () =>
-			import('./tabs/tabs.module').then(m => m.TabsPageModule),
-	},
-	//   { path: 'wallet', loadChildren: './pages/wallet/wallet.module#WalletPageModule' },
-	{
-		path: 'detail/:coin/:unit',
-		loadChildren:
-			'./pages/market-detail/market-detail.module#MarketDetailPageModule',
-	},
-	{
-		path: 'balance',
-		loadChildren: './pages/balance/balance.module#BalancePageModule',
-	},  { path: 'withdraw', loadChildren: './pages/withdraw/withdraw.module#WithdrawPageModule' },
+    {
+        path: '',
+        loadChildren: () =>
+            import('./tabs/tabs.module').then(m => m.TabsPageModule),
+    },
+    //   { path: 'wallet', loadChildren: './pages/wallet/wallet.module#WalletPageModule' },
+    {
+        path: 'detail/:coin/:unit',
+        loadChildren:
+            './pages/market-detail/market-detail.module#MarketDetailPageModule',
+    },
+    {
+        path: 'balance',
+        loadChildren: './pages/balance/balance.module#BalancePageModule',
+    },
+    {
+        path: 'withdraw',
+        loadChildren: './pages/withdraw/withdraw.module#WithdrawPageModule',
+    },  { path: 'deposit', loadChildren: './pages/deposit/deposit.module#DepositPageModule' },
 
 ];
 
 @NgModule({
-	imports: [
-		RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
-	],
-	exports: [RouterModule],
+    imports: [
+        RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+    ],
+    exports: [RouterModule],
 })
 export class AppRoutingModule {}
